377279a32a7d3def06524c85cd3d2c8f21be2033
Joseph <jdhallows.emc@gmail.com>
2018-10-28 15:38:10 -0400

    Add project files.

---
+++ EMCLiveMap/js/main.js
@@ -0,0 +1,57 @@
+﻿// Add active class to the current server link (highlight it)
+var header = document.getElementById("server_list");
+var btns = header.getElementsByClassName("server_link");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active_server");
+        current[0].className = current[0].className.replace(" active_server", "");
+        this.className += " active_server";
+    });
+}
+
+// Accordion menu for world selection
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active_world");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+// On click events
+document.getElementById("btn_town").addEventListener("click", function () {
+    var map = document.getElementById("smp_map");
+    var currentSMP = document.getElementsByClassName("active_server");
+    map.src = currentSMP[0].href + "&worldname=town";
+});
+
+document.getElementsByClassName("lnk_waste").addEventListener("click", function () {
+    var map = document.getElementById("smp_map");
+    var currentSMP = document.getElementsByClassName("active_server");
+    var waste_outpost_coords = none;
+    switch (this.id) {
+        case "lnk_waste_center":
+            waste_outpost_coords = "&x=0&z=0";
+            break;
+        case "lnk_waste_north":
+            waste_outpost_coords = "&x=0&z=-4000";
+            break;
+        case "lnk_waste_east":
+            waste_outpost_coords = "&x=4000&z=0";
+            break;
+        case "lnk_waste_south":
+            waste_outpost_coords = "&x=0&z=4000";
+            break;
+        case "lnk_waste_west":
+            waste_outpost_coords = "&x=-4000&z=0";
+            break;
+    }
+    map.src = currentSMP[0].href + "&worldname=wastelands" + waste_outpost_coords;
+});
